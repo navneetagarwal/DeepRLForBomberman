3376f2b83fd1bdcd07c3d64f5186e67fdc4a931b
Tanmay <tparekh97@gmail.com>
2017-11-04 22:00:12 +0530

    Fixed bugs for reflex agent

--- gameCode/agent.py
+++ gameCode/agent.py
@@ -18,29 +18,31 @@ class ReflexAgent:
 		self.config = config.Config()
 
 	def canMove(self, position, board, direction):
-		x = position[0]/self.config.TILE_SIZE
 		y = position[1]/self.config.TILE_SIZE
-		print ("Curr Pos -> ", (x,y))
+		x = position[0]/self.config.TILE_SIZE
+		print ("Curr Pos -> ", (y,x))
+		print (board[x-1][y].type, board[x+1][y].type, board[x][y-1].type, board[x][y+1].type)
+
 		if direction == "left":
-			# print (board[x-1][y])
+			# print (board[x-1][y].type)
 			if board[x-1][y].type != self.config.WALL and board[x-1][y].type != self.config.BRICK:
 				return 1
 			else:
 				return 0
 		elif direction == "right":
-			# print (board[x+1][y])
+			# print (board[x+1][y].type)
 			if board[x+1][y].type != self.config.WALL and board[x+1][y].type != self.config.BRICK:
 				return 1
 			else:
 				return 0
 		elif direction == "up":
-			# print (board[x][y-1])
+			# print (board[x][y-1].type)
 			if board[x][y-1].type != self.config.WALL and board[x][y-1].type != self.config.BRICK:
 				return 1
 			else:
 				return 0
 		elif direction == "down":
-			# print (board[x][y+1])
+			# print (board[x][y+1].type)
 			if board[x][y+1].type != self.config.WALL and board[x][y+1].type != self.config.BRICK:
 				return 1
 			else:
@@ -50,23 +52,39 @@ class ReflexAgent:
 			return 0
 
 	def getAction(self):
+
 		if self.state != None:
 			bombPositions = self.state.bombPositions
 			userPosition = self.state.userPosition
 			enemyPositions = self.state.enemyPositions
 			board = self.state.board.board
 
-			for bombPos in bombPositions:				
+			# User position
+			x = userPosition[1]/self.config.TILE_SIZE
+			y = userPosition[0]/self.config.TILE_SIZE
+
+			# Escape from bomb
+			for bombPos in bombPositions:
+				print (userPosition[0], userPosition[1])
+				print (bombPos[0], bombPos[1])				
 				if bombPos[0] == userPosition[0]:	# Same x-coordinate -> Move left or right
 					if self.canMove(userPosition, board, "left"):
 						return "left"
 					elif self.canMove(userPosition, board, "right"):
 						return "right"
-				elif bombPos[1] == userPosition[1]:	# Same y-coordinate -> Move up or down
+				if bombPos[1] == userPosition[1]:	# Same y-coordinate -> Move up or down
 					if self.canMove(userPosition, board, "up"):
 						return "up"
 					elif self.canMove(userPosition, board, "down"):
 						return "down"
+
+			# If near brick, then place bomb
+			if (board[x][y-1].type == self.config.BRICK or board[x][y+1].type == self.config.BRICK or board[x-1][y].type == self.config.BRICK or board[x+1][y].type == self.config.BRICK):
+				# print (x,y)
+				# print (board[x][y-1].type, board[x][y+1].type, board[x-1][y].type, board[x+1][y].type)
+				return "bomb"
+			else:
+				return 'up down left right stay'.split()[random.randint(0,4)]
 
 		return 'up down left right bomb stay'.split()[random.randint(0,5)]
 
