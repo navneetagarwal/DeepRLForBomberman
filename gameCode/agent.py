0ccc45365ec5823dbdfcd16902f8ae84abd4bd00
Tanmay <tparekh97@gmail.com>
2017-11-04 16:22:37 +0530

    Added basic code for reflex agent

--- gameCode/agent.py
+++ gameCode/agent.py
@@ -1,6 +1,5 @@
-import numpy as np
-import random
-import math
+import random, sys, math
+import config
 
 class RandomAgent:
 	def __init__(self):
@@ -15,13 +14,64 @@ class RandomAgent:
 
 class ReflexAgent:
 	def __init__(self):
-		pass
+		self.state = None
+		self.config = config.Config()
+
+	def canMove(self, position, board, direction):
+		x = position[0]/self.config.TILE_SIZE
+		y = position[1]/self.config.TILE_SIZE
+		print ("Curr Pos -> ", (x,y))
+		if direction == "left":
+			# print (board[x-1][y])
+			if board[x-1][y].type != self.config.WALL and board[x-1][y].type != self.config.BRICK:
+				return 1
+			else:
+				return 0
+		elif direction == "right":
+			# print (board[x+1][y])
+			if board[x+1][y].type != self.config.WALL and board[x+1][y].type != self.config.BRICK:
+				return 1
+			else:
+				return 0
+		elif direction == "up":
+			# print (board[x][y-1])
+			if board[x][y-1].type != self.config.WALL and board[x][y-1].type != self.config.BRICK:
+				return 1
+			else:
+				return 0
+		elif direction == "down":
+			# print (board[x][y+1])
+			if board[x][y+1].type != self.config.WALL and board[x][y+1].type != self.config.BRICK:
+				return 1
+			else:
+				return 0
+		else:
+			# Incorrect direction
+			return 0
 
 	def getAction(self):
-		pass
+		if self.state != None:
+			bombPositions = self.state.bombPositions
+			userPosition = self.state.userPosition
+			enemyPositions = self.state.enemyPositions
+			board = self.state.board.board
+
+			for bombPos in bombPositions:				
+				if bombPos[0] == userPosition[0]:	# Same x-coordinate -> Move left or right
+					if self.canMove(userPosition, board, "left"):
+						return "left"
+					elif self.canMove(userPosition, board, "right"):
+						return "right"
+				elif bombPos[1] == userPosition[1]:	# Same y-coordinate -> Move up or down
+					if self.canMove(userPosition, board, "up"):
+						return "up"
+					elif self.canMove(userPosition, board, "down"):
+						return "down"
+
+		return 'up down left right bomb stay'.split()[random.randint(0,5)]
 
 	def observe(self, newState, reward, event):
-		pass
+		self.state = newState
 
 class Agent(object):
 	def __init__(self, algorithm):
